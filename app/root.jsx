b09f794e8d6213019ac1232620c4d64cd0c3c8a6
Michael Chacon <mcljs15@gmail.com>
2022-09-29 12:43:29 -0400

    add Google Anality

--- app/root.jsx
+++ app/root.jsx
@@ -10,6 +10,7 @@ import {
   useLoaderData,
   useTransition,
 } from "@remix-run/react";
+import ReactGA from "react-ga";
 import { AnimatePresence, motion } from "framer-motion";
 import styles from "./styles/style.css";
 import { SsrTheme, ThemeMeta, ThemeProvider, useTheme } from "./utils/theme";
@@ -17,69 +18,66 @@ import { getThemeSession } from "./utils/theme-session.server";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
 import Navbar from "./components/Navbar";
-import { useSpinDelay } from 'spin-delay'
+import { useSpinDelay } from "spin-delay";
 import { NotificationMessage } from "./components/NotificationMessage";
 import { Circle } from "./components/Circle";
 import Footer from "./components/Footer";
 
-const LOADER_WORDS = [
-  'Cargando...',
- 
-]
+const LOADER_WORDS = ["Cargando..."];
 
 const ACTION_WORDS = [
-  'packaging',
-  'zapping',
-  'validating',
-  'processing',
-  'calculating',
-  'computing',
-  'computering',
-]
+  "packaging",
+  "zapping",
+  "validating",
+  "processing",
+  "calculating",
+  "computing",
+  "computering",
+];
 
 // we don't want to show the loading indicator on page load
-let firstRender = true
+let firstRender = true;
 
 function PageLoadingMessage() {
-  const transition = useTransition()
-  const [words, setWords] = React.useState([])
-  const [pendingPath, setPendingPath] = React.useState('')
-  const showLoader = useSpinDelay(Boolean(transition.state !== 'idle'), {
+  const transition = useTransition();
+  const [words, setWords] = React.useState([]);
+  const [pendingPath, setPendingPath] = React.useState("");
+  const showLoader = useSpinDelay(Boolean(transition.state !== "idle"), {
     delay: 400,
     minDuration: 1000,
-  })
+  });
 
   React.useEffect(() => {
-    if (firstRender) return
-    if (transition.state === 'idle') return
-    if (transition.state === 'loading') setWords(LOADER_WORDS)
-    if (transition.state === 'submitting') setWords(ACTION_WORDS)
+    if (firstRender) return;
+    if (transition.state === "idle") return;
+    if (transition.state === "loading") setWords(LOADER_WORDS);
+    if (transition.state === "submitting") setWords(ACTION_WORDS);
 
     const interval = setInterval(() => {
-      setWords(([first, ...rest]) => [...rest, first])
-    }, 2000)
+      setWords(([first, ...rest]) => [...rest, first]);
+    }, 2000);
 
-    return () => clearInterval(interval)
-  }, [pendingPath, transition.state])
-
-  React.useEffect(() => {
-    if (firstRender) return
-    if (transition.state === 'idle') return
-    setPendingPath(transition.location.pathname)
-  }, [transition])
+    return () => clearInterval(interval);
+  }, [pendingPath, transition.state]);
 
   React.useEffect(() => {
-    firstRender = false
-  }, [])
+    if (firstRender) return;
+    if (transition.state === "idle") return;
+    setPendingPath(transition.location.pathname);
+  }, [transition]);
 
-  const action = words[0]
+  React.useEffect(() => {
+    firstRender = false;
+  }, []);
+
+  const action = words[0];
 
   return (
     <NotificationMessage position="bottom-right" visible={showLoader}>
       <div className="flex w-64 items-center">
         <motion.div
-          transition={{repeat: Infinity, duration: 2, ease: 'linear'}}
-          animate={{rotate: 360}}
+          transition={{ repeat: Infinity, duration: 2, ease: "linear" }}
+          animate={{ rotate: 360 }}
         >
           <Circle size={48} team="UNKNOWN" />
         </motion.div>
@@ -88,21 +86,21 @@ function PageLoadingMessage() {
             <div className="col-start-1 row-start-1 flex overflow-hidden">
               <motion.span
                 key={action}
-                initial={{y: 15, opacity: 0}}
-                animate={{y: 0, opacity: 1}}
-                exit={{y: -15, opacity: 0}}
-                transition={{duration: 0.25}}
+                initial={{ y: 15, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -15, opacity: 0 }}
+                transition={{ duration: 0.25 }}
                 className="flex-none text-slate-100"
               >
                 {action}
               </motion.span>
             </div>
           </AnimatePresence>
-          <span className=" text-slate-200 truncate">path: {pendingPath}</span>
+          <span className=" truncate text-slate-200">path: {pendingPath}</span>
         </div>
       </div>
     </NotificationMessage>
-  )
+  );
 }
 
 export const links = () => {
@@ -112,10 +110,7 @@ export const links = () => {
   ];
 };
 
-
-
 export const loader = async ({ request }) => {
-  
   const { getTheme } = await getThemeSession(request);
   return json({
     user: await getUser(request),
@@ -125,15 +120,16 @@ export const loader = async ({ request }) => {
 
 function App() {
   const [theme] = useTheme();
+  React.useEffect(() => {
+    ReactGA.initialize("G-SG8RM3MQST");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <html lang="en" className={`h-full ${theme ? theme : "dark"}`}>
       <head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <ThemeMeta />
         <Links />
-        
       </head>
       <body className="duration-50 h-full bg-slate-100  text-slate-900 transition dark:bg-gray-900">
         <PageLoadingMessage />
--- app/utils/seo.js
+++ app/utils/seo.js
@@ -1,6 +1,8 @@
 import { initSeo } from "remix-seo";
 
 export const { getSeo, getSeoLinks, getSeoMeta } = initSeo({
+  charset: "utf-8",
+  viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
   title: "Misión Arbol",
   description: "Misión Árbol. MINEC. Gobierno Bolivariano de Venezuela",
   image: "https://misionarbol.minec.gob.ve/seoInit.jpeg",
